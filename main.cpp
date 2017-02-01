bc95db91b468f5e399de152b3f5d91df913af741
nathanblanchard12 <nathanblanchard12@gmail.com>
2017-02-01 16:18:51 -0500

    main.cpp
    split sentences that have spaces in c++

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+int main() {
+    std::string s = "scott tiger mushroom";
+    std::string delimiter = " ";
+
+    size_t pos = 0;
+    std::string token;
+    while ((pos = s.find(delimiter)) != std::string::npos) {
+        token = s.substr(0, pos);
+        std::cout << token << std::endl;
+        s.erase(0, pos + delimiter.length());
+    }
+    std::cout << s << std::endl;
+}
